ec9f681bdb20d4386091f6a7dcebb3cdd23bb365
TefoT4 <pmotatamali@gmail.com>
2025-01-13 16:08:13 +0200

    add context menu for AI explanation feature in background script

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,15 @@
 // TODO: background script
 chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+  chrome.contextMenus.create({
+    id: "AISearchMenu",
+    title: "Explain with AI",
+    contexts: ["selection"],
+  });
+
+  chrome.contextMenus.onClicked.addListener((event) => {
+    if (event.menuItemId == "AISearchMenu") {
+      const selectedText = event.selectionText;
+      console.log(selectedText);
+    }
+  });
+});
