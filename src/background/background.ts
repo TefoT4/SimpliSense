02af01a7898c8ae3ae183cce6c9d5848ac62ca9a
TefoT4 <pmotatamali@gmail.com>
2025-01-13 16:24:00 +0200

    refactor: rename project to "explaino" and update TypeScript configuration

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,14 +1,14 @@
 // TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((): void => {
   chrome.contextMenus.create({
     id: "AISearchMenu",
     title: "Explain with AI",
     contexts: ["selection"],
   });
 
-  chrome.contextMenus.onClicked.addListener((event) => {
-    if (event.menuItemId == "AISearchMenu") {
-      const selectedText = event.selectionText;
+  chrome.contextMenus.onClicked.addListener((event: chrome.contextMenus.OnClickData): void => {
+    if (event.menuItemId === "AISearchMenu") {
+      const selectedText: string | undefined = event.selectionText;
       console.log(selectedText);
     }
   });
